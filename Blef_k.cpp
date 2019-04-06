dcef36b2bf73bc4f11d6b2e4d68c72a38dbe5e8f
Jakub Kozak <49288612+MrKojak@users.noreply.github.com>
2019-04-06 19:42:23 +0200

    Add files via upload

--- Blef_k.cpp
+++ Blef_k.cpp
@@ -5,72 +5,82 @@
 #include <iostream>
 using namespace std;
 
-class player
+void menu()
+{
+    //cos
+}//ilosc graczy itp
+
+void start_gry()
+{
+    //cos
+}
+
+class Player
 {
 public:
-	string Imie;
+	string imie;
     int ilosc_k;
 	bool alive; //czy grasz wciaz "zyje" gra
-	
+    funkcja_sterujaca();
 
-	Life_player(string="random",int=1,bool=1);
-   ~Life_player();
-	
+	Player(string="random",int=1,bool=true);
+    ~Player();
+
 };
 
-class cards
+class Cards
 {
 public:
-	int cross=; //do porownywania wartosci z nazwami (jopek itp)
+	int cross; //do porownywania wartosci z nazwami (jopek itp)
     int wartosc;
 	bool alive; //czy grasz wciaz "zyje" gra
-	
 
-	T_card();
-   ~T_card();
+
+	Cards();
+    ~Cards();
 			draw(); //kto zaczyna ,kto juz nie gra (>5 -kart) itp
 			licytacja(); //interpreter kart, porownywanie , kto wygral (podbij/sprawdz) itp
-			random_card(); //tasowanie	
+			random_card(); //tasowanie
 };
 
-T_card::T_card()
+Cards::Cards()
 {
    	// cos
 };
 
-T_card::~T_card()
+Cards::~Cards()
 {
    	// cos
 };
 
 
-Life_player::Life_player(string name,int ilosc,bool a)
+Player::Player(string name,int ilosc,bool a)
 {
-	Imie=name;
+	imie=name;
 	ilosc_k=ilosc;
 	alive=a;
-	dynamiczna_tablica_kart_gracza(ilosc);
+	//dynamiczna_tablica_kart_gracza(ilosc); ????
 };
 
-Life_player::~Life_player()
+Player::~Player()
 {
    	// cos
 };
 
 void tasowanie()
 {
-	tablica_na_karty;
+	int tablica_na_karty[20];
 };
 
 
-void funkcja sterujaca()
-{	
-ilosc_graczy=0;
+void funkcja_sterujaca()
+{
+    int ilosc_graczy = 0;
 	menu(); //ilosc graczy itp
-	start gry()
+	start_gry();
 	{
-		player p1;
-		
+       Player p1;
+
     /*funkcje itp (cala gra)
     p1.draw();
     z1.licytacja();
@@ -84,10 +94,9 @@ ilosc_graczy=0;
 
 };
 
-
 int main()
 {
 	funkcja_sterujaca();
-	
-return 0;	
+
+return 0;
 }
