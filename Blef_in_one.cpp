860859bd116385c04fa480d8e80aa7709763447b
konda93 <46976995+konda93@users.noreply.github.com>
2019-04-08 15:38:22 +0200

    Add files via upload

--- Blef_in_one.cpp
+++ Blef_in_one.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int randomfunc(int j) 
+{ 
+    return rand() % j; 
+} 
 
 int main()
 {
@@ -30,7 +36,7 @@ int main()
 		ilosc_1_ogol=1; ilosc_2_ogol=1; ilosc_3_ogol=1; ilosc_4_ogol=1; 
 		cout<<"gracie w: "<<ilosc_graczy<<" graczy"<<endl; break;
 		case 5: 
-		ilosc_1_ogol=1; ilosc_2_ogol=1; ilosc_3_ogol=2; ilosc_4_ogol=1; ilosc_5_ogol=1; 
+		ilosc_1_ogol=1; ilosc_2_ogol=2; ilosc_3_ogol=2; ilosc_4_ogol=3; ilosc_5_ogol=3; 
 		cout<<"gracie w: "<<ilosc_graczy<<" graczy"<<endl; break;
 	}
 	int reka1[5];
@@ -38,32 +44,40 @@ int main()
 	int reka3[5];
 	int reka4[5];
 	int reka5[5];
-	srand( time( NULL ) );
-	int deck[24]={9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14}; //tablica kart
+	//srand( time( NULL ) );
+//	int deck[24]={9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14}; //tablica kart
 	
 do//glowna funkcja sterujaca
 {	
 
 	int ilosc_kart=ilosc_1_ogol+ilosc_2_ogol+ilosc_3_ogol+ilosc_4_ogol+ilosc_5_ogol;
-	int licznik=0;//licznik
-	for(int i=0;i<15;i++)//tasowanie-14 razy
-	{
-		int j=0;
-    	int x=0;
-		for(int i=0; i<24; i++) 
-    	{	
-    		j = rand()%23;
-    		x=deck[j];
-   			deck[j]=deck[i];
-       	 	deck[i]=x;
-        
-       	 	if(licznik==14) 
-				{
-					cout<<i+1<<":"<<deck[i]<<endl;
-				}
-    	}
-    	licznik++;
-	} 
+	
+srand(unsigned(time(0)));  //tasowanie
+    vector<int> arr; 
+   
+    for(int i=0,k=9;i<6;++i)
+    {
+    	for (int j = 0; j < 4; ++j) 
+  		{
+  			arr.push_back(k);	
+  		}
+  		k++;
+	}
+    // using built-in random generator 
+    random_shuffle(arr.begin(), arr.end()); 
+  
+    // using randomfunc 
+    random_shuffle(arr.begin(), arr.end(), randomfunc); 
+  
+    // print out content: 
+    cout << "Potasowany deck: "; 
+    for (vector<int>::iterator i = arr.begin(); i != arr.end(); ++i) 
+        cout << ' ' << *i; 
+  
+    cout << endl; 
+    
+
+
 
 
 cout<<"Gra: "<<ilosc_graczy<<" graczy"<<endl;
@@ -75,56 +89,56 @@ for(int karty_rozdane=0,g1=0,g2=0,g3=0,g4=0,g5=0;ilosc_1_now+ilosc_2_now+ilosc_3
 {
 	if(ilosc_1_now<ilosc_1_ogol)//1 gracz
 	{
-		reka1[g1]=deck[g1+g2+g3+g4+g5];
+		reka1[g1]=arr[g1+g2+g3+g4+g5];
 		cout<<"Gracz 1:"<<reka1[g1]<<endl;
 		g1++;
 		ilosc_1_now++;
 		karty_rozdane++;	
 	}
 	//ilosc_graczy_now++;
-	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
+//	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
 	if (karty_rozdane>=ilosc_kart) break;
 
 	if(ilosc_2_now<ilosc_2_ogol)//2 gracz
 	{
 	//if(ilosc_graczy_now==ilosc_graczy)	break;
-		reka2[g2]=deck[g1+g2+g3+g4+g5];
+		reka2[g2]=arr[g1+g2+g3+g4+g5];
 		cout<<"Gracz 2:"<<reka2[g2]<<endl;
-		cout<<"Gracz 2:"<<reka2[g2+1]<<endl;
+	//	cout<<"Gracz 2:"<<reka2[g2+1]<<endl;
 		g2++;
 		ilosc_2_now++;
 		karty_rozdane++;
 		
 	}
 //	ilosc_graczy_now++;
-	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
+//	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
 
 	if(ilosc_3_now<ilosc_3_ogol)//3 gracz
 	{
-		reka3[g3]=deck[g1+g2+g3+g4+g5];
+		reka3[g3]=arr[g1+g2+g3+g4+g5];
 		cout<<"Gracz 3:"<<reka3[g3]<<endl;
 		g3++;
 		ilosc_3_now++;
 		karty_rozdane++;
 	}
 	//ilosc_graczy_now++;
-	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
+//	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
 	
 //if(ilosc_graczy_now==ilosc_graczy)	break;
 	if(ilosc_4_now<ilosc_4_ogol)//4 gracz
 	{
-		reka4[g4]=deck[g1+g2+g3+g4+g5];
+		reka4[g4]=arr[g1+g2+g3+g4+g5];
 		cout<<"Gracz 4:"<<reka4[g4]<<endl;
 		g4++;
 		ilosc_4_now++;
 		karty_rozdane++;
 	}
 //	ilosc_graczy_now++;
-	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
+//	cout<<"Ilosc kart rozdanych: "<<karty_rozdane<<endl;
 //if(ilosc_graczy_now==ilosc_graczy)	break;
 	if(ilosc_5_now<ilosc_5_ogol)//5 gracz
 	{
-		reka5[g5]=deck[g1+g2+g3+g4+g5];
+		reka5[g5]=arr[g1+g2+g3+g4+g5];
 		cout<<"Gracz 5:"<<reka5[g5]<<endl;
 		g5++;
 		ilosc_5_now++;
